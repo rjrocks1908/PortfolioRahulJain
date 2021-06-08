004d6a825efb786242b31bb78bb74b047639ef20
unknown <rj.rahul1908@gmail.com>
2021-06-08 22:43:08 +0530

    sixth Commit

--- form.php
+++ form.php
@@ -5,14 +5,16 @@
 
     $email_from = 'MyPortfolio'
     $email_subject = $_POST['subject']
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+    $email_body = "User Name: ".$name."\n".
+                    "User Email: ".$visitor_email."\n".
+                        "User Message: ".$message."\n";
     
     $to = "rj.rahul1908@gmail.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
+    $headers = "From: " .$email_from. "\r\n";
+    $headers .= "Reply-To: ".$visitor_email."\r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
+    @mail($to,$email_subject,$email_body,$headers);
 
     header("Location: index.html");
+    
+    ?>
