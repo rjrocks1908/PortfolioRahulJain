521e234b805e53cb5bdf8cd0118ba2c577a173f9
unknown <rj.rahul1908@gmail.com>
2021-06-08 23:37:18 +0530

    eight Commit

--- script.js
+++ script.js
@@ -62,3 +62,28 @@ $(document).ready(function(){
     })
     
 });
+
+
+var form = document.getElementById("my-form");
+// var status = document.getElementById("status");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.classList.add('success');
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      }).catch(error => {
+        status.classList.add('error');
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
