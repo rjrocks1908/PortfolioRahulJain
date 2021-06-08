35c6e156bbcca8430455c7de9d8d3883582ed65e
unknown <rj.rahul1908@gmail.com>
2021-06-08 22:22:23 +0530

    fifth Commit

---
+++ form.php
@@ -0,0 +1,18 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $email_from = 'MyPortfolio'
+    $email_subject = $_POST['subject']
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\n";
+    
+    $to = "rj.rahul1908@gmail.com";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: index.html");
